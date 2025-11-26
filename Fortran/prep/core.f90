ab451d0f7caa0ea0bac263cb29a2458621173772
Milan Skocic <milan.skocic@gmail.com>
2025-11-26 06:48:19 +0100

    Add snippet for prep preprocessor.
    prep from github.com/urbanjost/prep.

---
+++ Fortran/prep/core.f90
@@ -0,0 +1,35 @@
+!>
+!!##NAME
+!!     core - Elementary module
+!!##SYNOPSIS
+!!
+!!     core(3f)
+!!##DESCRIPTION
+!!     Compute the core physics of a nuclear power plant
+!!     $ \alpha $
+module core
+
+    integer, parameter :: x = 1
+
+
+contains
+
+!>
+!!##NAME
+!!     f(3f) - test function
+!> @brief f function
+!! @param[in] x input
+function f(x)result(r)
+    integer, intent(in) :: x
+    integer :: r
+
+    r=x+1
+end function
+
+integer function g()
+! ident_1="@(#) core g(3f) function to copy char array to string"
+end function
+
+end module
+
+
---
+++ Fortran/prep/test.md
@@ -0,0 +1,8 @@
+
+```Fortran
+function ftest(x)result(r)
+    integer, intent(in) :: x
+    integer :: r
+    r  = x+2
+end function    
+```
