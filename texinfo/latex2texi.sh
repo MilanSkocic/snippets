2232cf5b6dd4d79be678ea4b2f49f1ac49d1f1a7
Milan Skocic <milan.skocic@gmail.com>
2025-12-12 06:39:08 +0100

    Start implementing a latex to texi converter.

--- texinfo/latex2texi.sh
+++ texinfo/latex2texi.sh
@@ -1,12 +1,38 @@
 #!/usr/bin/env bash
-
+set -x
 lATEXFILE=$1
 TEXIFILE="$(dirname $lATEXFILE)/$(basename -s .tex $lATEXFILE).texi"
 IFS=$'\n'
-convert () {
-    l=$1
 
-    echo $l
+convert () {
+    line=$1
+    line=$(echo $line | sed -E 's/^ *//g' | sed -E 's/^\t//g')
+
+    case $line in
+        "\\"*)
+            cmd=$(echo $line | grep -o -P '(?<=\\).*(?={)')
+            line=$(echo $line | sed -E 's/\\/\@/g')
+            line=$(echo $line | sed -E 's/\{/ /g')
+            line=$(echo $line | sed -E 's/\}/ /g')
+            line=$(echo $line | sed -E 's/\@label/\@node/g')
+            echo $line
+            ;;
+        *"$"*)
+            math=$(echo $line | grep -o -P '\$.*?\$')
+            f=$(echo $line | grep -o -P '(?<=\$).*(?=\$)')
+            f="\@math\{$f\}"
+            line=$(echo $line | sed -E "s:$math:$f:g")
+            echo $line
+            ;;
+        *)
+            line=$(echo $line | sed -E 's/\{/ /g')
+            line=$(echo $line | sed -E 's/\}/ /g')
+            line=$(echo $line | sed -E 's/\\tt//g')
+            line=$(echo $line | sed -E 's/\\rm//g')
+            line=$(echo $line | sed -E 's/\\bf//g')
+            echo $line
+            ;;
+    esac
 }
 
 
@@ -16,5 +42,5 @@ echo "@c Autogeneraed $date" > $TEXIFILE
 
 while read -r line; do
     l2=$(convert "$line")
-    echo $l2
+    echo $l2 >> $TEXIFILE
 done < $lATEXFILE
