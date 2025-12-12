4b7511160aa8c32d8c703884f5af66e93762f4c6
Milan Skocic <milan.skocic@gmail.com>
2025-12-12 21:54:35 +0100

    Update convert latex to texi.
    Chapter, section, labels, inline math, figures.

--- texinfo/latex2texi.sh
+++ texinfo/latex2texi.sh
@@ -1,38 +1,81 @@
 #!/usr/bin/env bash
-set -x
+#set -x
 lATEXFILE=$1
 TEXIFILE="$(dirname $lATEXFILE)/$(basename -s .tex $lATEXFILE).texi"
 IFS=$'\n'
 
 convert () {
     line=$1
-    line=$(echo $line | sed -E 's/^ *//g' | sed -E 's/^\t//g')
+    # line=$(echo $line | sed -E 's/^ *//g' | sed -E 's/^\t//g')
+
+    line=$(echo $line | sed -E 's/\\textbf/\@b/g')
+    line=$(echo $line | sed -E 's/\\textit/\@i/g')
+    line=$(echo $line | sed -E 's/\\emph/\@i/g')
+    line=$(echo $line | sed -E 's/\\texttt/\@t/g')
+    line=$(echo $line | sed -E 's/\\underline/\@i/g')
+    
+    math=$(echo $line | grep -o -P '(?<=\$).*(?=\$)')
+    if [[ "${#math}" > 0 ]]; then
+        echo "@c $line"
+        return 1
+    fi
 
     case $line in
+        "\\chapter"*)
+            line=$(echo $line | sed -E 's/\{/ /g')
+            line=$(echo $line | sed -E 's/\}/ /g')
+            line=$(echo $line | sed -E 's/\\chapter/\@chapter/g')
+            echo $line
+            ;;
+        *"\\section"*)
+            line=$(echo $line | sed -E 's/\{/ /g')
+            line=$(echo $line | sed -E 's/\}/ /g')
+            line=$(echo $line | sed -E 's/\\section/\@section/g')
+            echo $line
+            ;;
+        "\\label"*)
+            line=$(echo $line | sed -E 's/\{/ /g')
+            line=$(echo $line | sed -E 's/\}/ /g')
+            line=$(echo $line | sed -E 's/\\label/\@node/g')
+            echo $line
+            ;;
+        *"\\begin{figure}"*|*"\\end{figure}"*)
+            echo "@c $line"
+            ;;
+        *"\\centering"*)
+            echo "@c $line"
+            ;;
+        *"\\includegraphics["*|*"\includegraphics["*)
+            image=$(echo $line | grep -o -P '(?<=\{).*(?=\})')
+            image=$(echo $image | cut -d '.' -f 1)
+            echo "@image{$image,,,,.txt}"
+            ;;
+        *"\\caption"*)
+            echo "@c $line"
+            ;;
+        *"\\label"*)
+            echo "@c $line"
+            ;;
         "\\"*)
-            cmd=$(echo $line | grep -o -P '(?<=\\).*(?={)')
+            #cmd=$(echo $line | grep -o -P '(?<=\\).*(?={)')
             line=$(echo $line | sed -E 's/\\/\@/g')
             line=$(echo $line | sed -E 's/\{/ /g')
             line=$(echo $line | sed -E 's/\}/ /g')
             line=$(echo $line | sed -E 's/\@label/\@node/g')
+            line=$(echo $line | sed -E 's/\\includegraphics/\@image/g')
             echo $line
             ;;
-        *"$"*)
-            math=$(echo $line | grep -o -P '\$.*?\$')
-            f=$(echo $line | grep -o -P '(?<=\$).*(?=\$)')
-            f="\@math\{$f\}"
-            line=$(echo $line | sed -E "s:$math:$f:g")
+        *"\("*)
+            line=$(echo $line | sed -E 's/\\\(/@math\{/g')
+            line=$(echo $line | sed -E 's/\\\)/\}/g')
             echo $line
             ;;
         *)
-            line=$(echo $line | sed -E 's/\{/ /g')
-            line=$(echo $line | sed -E 's/\}/ /g')
-            line=$(echo $line | sed -E 's/\\tt//g')
-            line=$(echo $line | sed -E 's/\\rm//g')
-            line=$(echo $line | sed -E 's/\\bf//g')
             echo $line
             ;;
     esac
+    
+    return 0
 }
 
 
@@ -42,5 +85,6 @@ echo "@c Autogeneraed $date" > $TEXIFILE
 
 while read -r line; do
     l2=$(convert "$line")
+    echo "[INFO]: $l2"
     echo $l2 >> $TEXIFILE
 done < $lATEXFILE
