b55910d1051f1834f1bd10a74d93defcc58350d6
Milan Skocic <milan.skocic@gmail.com>
2025-12-27 18:41:44 +0100

    Add snippets for mimicing argp using stdlib strings and system.

--- Fortran/command_line_args/app/main.f90
+++ Fortran/command_line_args/app/main.f90
@@ -1,6 +1,14 @@
 program main
+    use stdlib_ansi, only : fg_color_blue, style_bold, style_reset, ansi_code, &
+    & operator(//), operator(+)
+    use stdlib_string_type
+    use stdlib_strings
+    use stdlib_stringlist_type
+    use stdlib_str2num, only: to_num
+    use stdlib_kinds, only: sp, dp
     implicit none
-
+    
+    ! Parameters
     integer, parameter :: buffer_size = 256
     character(len=*), parameter :: VERSION = "0.1"
     character(len=*), parameter :: PROGRAM_NAME = "fapp"
@@ -8,37 +16,44 @@ program main
     character(len=*), parameter :: SHORT_DESCRIPTION = PROGRAM_NAME//' - '//'Test program in Fortran.'
     character(len=*), parameter :: DESCRIPTION = 'This text goes under the options.'
     character(len=*), parameter :: EMAIL = '<milan.skocic@gmail.com>'
-    
+   
+    ! variables
+    type(ansi_code) :: highlight 
     integer :: iostat
-
     character(len=buffer_size) :: arg
     integer :: i
-    real :: r
+    real(dp) :: r
     character(len=64) :: s
+
+    type(string_type) :: myarg
+    type(stringlist_type)       :: options
+    type(stringlist_type)       :: args
+    type(stringlist_index_type) :: index
+    
+    highlight = fg_color_blue + style_bold
 
 
     do i=1, command_argument_count()
         call get_command_argument(i, arg)
+        myarg=strip(string_type(arg))
+        if(starts_with(myarg, "-") .eqv. .true.) then 
+            call options%insert_at(bidx(1), myarg) 
+        else
+            call args%insert_at(bidx(1), myarg)
+        end if
         select case(arg)
             case ("-V", "--version")
                 call print_version()
-                stop
             case ("-?", "--help")
                 call print_help()
-                stop
             case default
-                read (arg, "(F10.1)", iostat=iostat) r
-                if(iostat == 0)then 
-                    print *, r
-                    stop
-                end if
-                read (arg, "(A)", iostat=iostat) s
-                if(iostat == 0)then 
-                    print *, s
-                    stop
-                end if
+                r = to_num(arg, r)
+                print *, r
         end select
     end do
+
+    call display_string_list(options)
+    call display_string_list(args)
 
 
 contains
@@ -63,6 +78,16 @@ subroutine print_version()
     print '(A)',    'MIT License'
     print '(A)',    ''
     print '(A)',    'Written by '//AUTHOR
+end subroutine
+
+
+subroutine display_string_list(strlist)
+    type(stringlist_type), intent(in) :: strlist
+
+    integer :: i
+    do i=1, strlist%len(), 1
+        print '(I2, 4X, A)', i, char(strlist%get(fidx(i)))
+    end do
 end subroutine
     
 end program main
