a52848579afb5fa90d730e70eb3cef470c21df42
Milan Skocic <milan.skocic@gmail.com>
2026-03-28 18:57:35 +0100

    Update progress bar with 2 options length and character for the progress bar.

--- Fortran/progressbar/src/main.f90
+++ Fortran/progressbar/src/main.f90
@@ -1,23 +1,25 @@
 program main
-  use iso_fortran_env, only: int32, real32, real64, output_unit
-  use progress, only: display_progress
-  implicit none
-  
-  ! lest integrate a progress bar in the main code
-  integer(int32):: i
-  real(real64):: x, start, end, p
+use, intrinsic :: iso_fortran_env, only: int32, real32, real64, output_unit
+use progress, only: display_progress
+implicit none(type, external)
 
-  x = huge(x)
-  print *, x
-  call cpu_time(start)
-  do i = 1, huge(i), 1000
-    x = x/1.00000001d0
-    p = real(i, kind = real64)/real(huge(i), kind = real64)*100
-    call display_progress(real(p, kind = real32), 100)
-  end do
-  write(output_unit, *) ""
-  call cpu_time(end)
+! lest integrate a progress bar in the main code
+integer(int32):: i, N
+real(real64):: x, start, end, p
 
-  print *, end-start
+N = 20000000
+
+call cpu_time(start)
+
+do i = 1, N, 1
+p = real(i, kind = real64) / real(N,kind=real64)*100
+call display_progress(x=real(p, kind = real32), n=72)
+end do
+
+write(output_unit, *) ""
+
+call cpu_time(end)
+
+print *, end-start
 
 end program main
--- Fortran/progressbar/src/progress.f90
+++ Fortran/progressbar/src/progress.f90
@@ -1,32 +1,38 @@
 module progress
-    use iso_fortran_env, only: real32, int32, output_unit
-    implicit none
-    private
-    public:: display_progress
+use, intrinsic :: iso_fortran_env, only: real32, int32, output_unit
+implicit none(type, external)
+private
+public:: display_progress
+
 contains
 
-subroutine display_progress(x, n)
-    !! Display a progress bar.
-    implicit none
-    real(real32), intent(in):: x
-        !! Percentage of progress.
-    integer(int32), intent(in):: n
-        !! Length of the progressbar.
+subroutine display_progress(x, n, c)
+!! Display a progress bar.
+real(real32), intent(in):: x    !! Percentage of progress.
+integer(int32), intent(in):: n  !! Length of the progressbar.
+character(len=1), intent(in),  optional :: c
 
-    character(len = n):: progressbar
-    character(len = n+4):: progress
-    integer(int32):: p
-    integer(int32):: prel
-    character(len = 4):: pstr 
+character(len = n-5):: progressbar
+character(len = n):: progress
+integer(int32):: p
+integer(int32):: prel
+character(len = 5):: pstr
+character(len=1) :: m
 
-    p = nint(x, kind = int32)
-    prel = p*n / 100
-    write(pstr, "(I3, A)") p, "%"
-    progressbar = repeat("=", prel)
-    progress = progressbar//pstr 
-    write(output_unit, "(A1, A)", advance="NO") achar(13), progress
-    flush(output_unit)
+if(present(c)) then
+    m = c
+else
+    m = '='
+end if
 
-end subroutine
+p = nint(x, kind=int32)
+prel = p*n / 100
+write(pstr, "(1X, I3, A)") p, "%"
+progressbar = repeat(m, prel)
+progress = progressbar//adjustr(pstr)
+write(output_unit, "(A1,A)", advance="NO") achar(13), progress
+flush(output_unit)
 
-end module
+end subroutine display_progress
+
+end module progress
