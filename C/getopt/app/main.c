6456ef59ab957ca0ab77b2ae54cd4bb9a56c5da2
Milan Skocic <milan.skocic@gmail.com>
2026-06-26 21:11:38 +0200

    Add example with long options.

--- C/getopt/app/main.c
+++ C/getopt/app/main.c
@@ -7,12 +7,39 @@
 #include <error.h>
 #include "kTe.h"
 
-char *version=
-"kTe 1.0.1\n"
-"\n"
-"Copyright (C) 2026 Milan Skocic\n"
-"\n"
-"Written by Milan Skocic.\n";
+static char VERSION_TEXT[256];
+
+struct option_t {
+    char *s; // long option
+    char *l; // short option
+    char *help;
+};
+
+char *version(){return "1.0.0";}
+
+static struct option_t loptions[]={
+    {"-T", "--temperature", "Temperatures."},
+    {"-v", "--version",     "Display version and exit."},
+    {"-u", "--usage",       "Display usage and exit."},
+    {"-h", "--help",        "Display help and exit."},
+    {NULL,        NULL, NULL}
+};
+
+static char *long2short(char *option, struct option_t *options){
+    int i=0;
+    
+    // Check option structure. If incorrect return NULL.
+    if(option==NULL){return NULL;}
+    if(strlen(option)<3){return option;}
+    if(!((option[0]=='-') && (option[1]=='-'))){return option;}
+
+    while(options[i].l!=NULL){
+        if(strcmp(option, options[i].l)==0){return options[i].s;}
+        else{i++;}
+    }
+    return option;
+}
+
 
 struct array_t{
     double *data;
@@ -25,7 +52,6 @@ void array_print(struct array_t self){
 	for(i=0;i<self.n;i++){
 		printf("%-+10.2f\n", *(self.data+i));
 	}
-	
 }
 
 void array_table(struct array_t **x, int n, int m){
@@ -56,12 +82,10 @@ int size(char *s){
     }
     free(buf);
     return i;
-
 }
 
 
 void s2d(struct array_t x, char * restrict s){
-    
     int i = 0;
 
     char *buf = (char *)malloc(sizeof(char)*strlen(s));
@@ -84,8 +108,16 @@ void s2d(struct array_t x, char * restrict s){
 int main(int argc, char** argv){
     size_t i;
     int opt;
+    char *s;
     struct array_t T={NULL,0, &array_print};
 	struct array_t res={NULL,0, &array_print};
+
+    strcat(VERSION_TEXT, "codata ");
+    strcat(VERSION_TEXT, version());
+    strcat(VERSION_TEXT, "\n\n");
+    strcat(VERSION_TEXT, "Copyright (C) 2026 Milan Skocic\n");
+    strcat(VERSION_TEXT, "\n\n");
+    strcat(VERSION_TEXT, "Written by Milan Skocic.\n");
 	
 	struct array_t **arrays=(struct array_t **)malloc(sizeof(struct array_t *)*2);
 	arrays[0] = &T;
@@ -96,6 +128,11 @@ int main(int argc, char** argv){
         return EXIT_FAILURE;
     }
     args[0]='\0';
+    
+    for(i=1;i<argc;i++){
+        s = long2short(argv[i], loptions);
+        if(s!=NULL){argv[i]=s;}
+    }
 
     while ((opt = getopt(argc, argv, ":T:vh")) != -1) {
         switch (opt) {
@@ -107,7 +144,7 @@ int main(int argc, char** argv){
             i++;
             break;
         case 'v':
-            printf("%s", version);
+            printf("%s", VERSION_TEXT);
             return EXIT_SUCCESS;
             break;
         case ':': 
